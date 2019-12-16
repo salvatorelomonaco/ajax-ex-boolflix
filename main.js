19683a5640fea5aca0183507280d421d0c201ed4
salvatorelomonaco <lommetto95@gmail.com>
2019-12-16 16:14:05 +0100

    Aggiunte serie Tv

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
 
     $('button').click(function () {
         searchMovie();
+        searchSeriesTV();
     });
 
     $('.search').keypress(function(event) {
     // il pulsante invia equivale a 13
         if(event.which == 13 ){
             searchMovie();
+            searchSeriesTV();
         };
     });
 
@@ -41,7 +43,34 @@ $(document).ready(function() {
         });
     };
 
-    // creo una funzione che mi va a leggere le info che voglio dalla mia api
+    // funzione per ricercare le serieTv
+    function searchSeriesTV(ricercaUtente) {
+        var ricercaUtente = $('.header-right input').val();
+        $.ajax({
+            // uso url della api di themoviedb
+            'url': 'https://api.themoviedb.org/3/search/tv',
+            // allego questi dati all'url
+            'data': {
+                // la mia chiave api
+                'api_key': '8f20fb07349b7328a5884d56f17b612d',
+                // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
+                'query': ricercaUtente,
+                // la lingua italiana
+                'language': 'it-IT'
+            },
+            'method': 'GET',
+            'success': function(data) {
+                // richiamo la funzione delle informazione del film
+                infoSeriesTv(data.results);
+            },
+            // in caso di errore
+            'error': function() {
+                alert('Error');
+            }
+        });
+    };
+
+    // creo una funzione per estrarre le informazione dei film dall'api
     function infoMovie(movie) {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
         for (var i = 0; i < movie.length; i++) {
@@ -64,4 +93,28 @@ $(document).ready(function() {
             $('.film-container').append(html);
         };
     };
+
+    // creo una funzione per estrarre le informazione delle serieTv dall'api
+    function infoSeriesTv(seriesTv) {
+        // uso un ciclo for visto che mi verrà restituita un array di oggetti
+        for (var i = 0; i < seriesTv.length; i++) {
+            // creo le varie variabili per andare a prendere i dati di cui ho bisogno
+            var title = seriesTv[i].name;
+            var originalTitle = seriesTv[i].original_name;
+            var language = seriesTv[i].original_language;
+            // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
+            var vote = (Math.ceil(seriesTv[i].vote_average)) / 2;
+            // info da sostituire nel mio handlebars template
+            var info = {
+                'title': title,
+                'original-title': originalTitle,
+                'language': language,
+                'vote': vote
+            };
+            // creo una variabile che mi compili le info con una funzione
+            var html = templateFunction(info);
+            //  le appendo al container
+            $('.film-container').append(html);
+        };
+    }
 });
