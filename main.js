05055e5f515ec1e8ddee37733676d6954b9f8da3
salvatorelomonaco <lommetto95@gmail.com>
2019-12-16 12:54:04 +0100

    Milestone 1 quasi completa, manca snelline il codice con delle funzioni

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
         $.ajax({
             'url': 'https://api.themoviedb.org/3/search/movie',
             'data': {
-                'api_key': 'e99307154c6dfb0b4750f6603256716d',
-                'query': letturaRicerca
+                'api_key': '8f20fb07349b7328a5884d56f17b612d',
+                'query': letturaRicerca,
+                'language': 'it-IT'
             },
             'method': 'GET',
             'success': function(data) {
                 var film = data.results
-                console.log(film);
                 for (var i = 0; i < film.length; i++) {
                     var title = film[i].title;
                     var originalTitle = film[i].original_title;
@@ -31,11 +31,30 @@ $(document).ready(function() {
 
                     $('.film-container').append(html);
                 }
-
             },
             'error': function() {
-                alert('Error')
+                alert('Error');
             }
         });
     });
 });
+
+//
+// function printMovie(movie, info) {
+//     for (var i = 0; i < movie.length; i++) {
+//         var title = movie[i].title;
+//         var originalTitle = movie[i].original_title;
+//         var language = movie[i].original_language;
+//         var vote = movie[i].vote_average;
+//         var info = {
+//             'title': title,
+//             'original-title': originalTitle,
+//             'language': language,
+//             'vote': vote
+//         };
+//
+//         var html = templateFunction(info);
+//
+//         $('.film-container').append(html);
+//     }
+// }
