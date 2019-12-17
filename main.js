0ce9feb000f9005e28d920b38acf1dad5f684145
salvatorelomonaco <lommetto95@gmail.com>
2019-12-17 17:48:05 +0100

    c'è un problema nel richiamo delle serie tv

--- main.js
+++ main.js
@@ -63,6 +63,7 @@ $(document).ready(function() {
                         'success': function(data) {
                             // richiamo la funzione delle informazione della serie tv
                             infoSeriesTv(data.results);
+                            console.log(data.results);
                         },
                         // in caso di errore
                         'error': function() {
@@ -113,31 +114,38 @@ $(document).ready(function() {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
         for (var i = 0; i < movie.length; i++) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-            if (movie[i].poster_path != null){
-                var title = movie[i].title;
-                var originalTitle = movie[i].original_title;
-                var language = movie[i].original_language;
-                // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
-                var vote = (Math.ceil(movie[i].vote_average)) / 2;
-                var img = movie[i].poster_path;
-                var description = movie[i].overview;
-                // info da sostituire nel mio handlebars template
-                var info = {
-                    'title': title,
-                    'original-title': originalTitle,
-                    'language': language,
-                    'vote': vote,
-                    'poster': img,
-                    'overview': description
+            // if (movie[i].poster_path != null){
+            var title = movie[i].title;
+            var originalTitle = movie[i].original_title;
+            var language = movie[i].original_language;
+            // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
+            var voto = Math.ceil((movie[i].vote_average) / 2);
+            console.log(voto);
+            var stellaPiena = '<i class="fas fa-star"></i>';
+            var stellaVuota = '<i class="far fa-star"></i>';
+            var stellePiene = '';
+                for (var j = 0; j < 5; j++) {
+                    if (j < voto) {
+                        stellePiene = stellePiene + stellaPiena;
+                    } else {
+                        stellePiene = stellePiene + stellaVuota;
+                    }
                 };
-                // creo una variabile che mi compili le info con una funzione
-                var html = templateFunction(info);
-                //  le appendo al container
-                $('.film-container').append(html);
-
-            } else {
-                !$('.film-container').append(html);
-            }
+            var img = movie[i].poster_path;
+            var description = movie[i].overview;
+            // info da sostituire nel mio handlebars template
+            var info = {
+                'title': title,
+                'original-title': originalTitle,
+                'language': language,
+                'vote': stellePiene,
+                'poster': img,
+                'overview': description
+            };
+            // creo una variabile che mi compili le info con una funzione
+            var html = templateFunction(info);
+            //  le appendo al container
+            $('.film-container').append(html);
         };
     };
 
@@ -145,32 +153,43 @@ $(document).ready(function() {
     function infoSeriesTv(seriesTv) {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
         for (var i = 0; i < seriesTv.length; i++) {
-            if (seriesTv[i].poster_path != null) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-                var title = seriesTv[i].name;
-                var originalTitle = seriesTv[i].original_name;
-                var language = seriesTv[i].original_language;
-                // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
-                var vote = (Math.ceil(seriesTv[i].vote_average)) / 2;
-                var img = seriesTv[i].poster_path;
-                var description = seriesTv[i].overview;
-                // info da sostituire nel mio handlebars template
-                var info = {
-                    'title': title,
-                    'original-title': originalTitle,
-                    'language': language,
-                    'vote': vote,
-                    'poster': img,
-                    'overview': description
-                };
-                // creo una variabile che mi compili le info con una funzione
-                var html = templateFunction(info);
-                //  le appendo al container
-                $('.film-container').append(html);
-
-            } else {
-                !$('.film-container').append(html);
-            }
+            var title = seriesTv[i].name;
+            var originalTitle = seriesTv[i].original_name;
+            var language = seriesTv[i].original_language;
+            // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
+            var voto = Math.ceil((seriesTv[i].vote_average) / 2);
+            console.log(voto);
+            var stellaPiena = '<i class="fas fa-star"></i>';
+            var stellaVuota = '<i class="far fa-star"></i>';
+            var stelle;
+            for (var j = 0; j < 5; j++) {
+                if (j > voto) {
+                    stelle = stelle + stellaPiena;
+                } else {
+                    stelle = stelle + stellaVuota;
+                }
+            };
+            var img = seriesTv[i].poster_path;
+            var description = seriesTv[i].overview;
+            // info da sostituire nel mio handlebars template
+            var info = {
+                'title': title,
+                'original-title': originalTitle,
+                'language': language,
+                'vote': stelle,
+                'poster': img,
+                'overview': description
+            };
+            // creo una variabile che mi compili le info con una funzione
+            var html = templateFunction(info);
+            //  le appendo al container
+            $('.film-container').append(html);
         };
     };
 });
+
+
+// devo trasformare il voto in stelle
+// ho già il Voto
+// dobbiamo trasformare il numero massimo di stelline in  icone
