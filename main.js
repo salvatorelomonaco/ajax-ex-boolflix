b6a97457b3c55fea2a2d73fe54fd6848e8c21754
salvatorelomonaco <lommetto95@gmail.com>
2019-12-17 18:40:40 +0100

    mancano le bandierine

--- main.js
+++ main.js
@@ -1,120 +1,89 @@
 $(document).ready(function() {
 
-    var templateFunction = Handlebars.compile($('#template').html());
+
 
     $('button').click(function () {
+        $('.film-container').empty();
+        var ricercaUtente = $('.header-right input').val();
         // uso .hide per resettare la ricerca ogni volta
-        searchMovieSeriesTv();
+        searchMovieSeriesTv(ricercaUtente);
     });
 
     $('.search').keypress(function(event) {
+        $('.film-container').empty();
+        var ricercaUtente = $('.header-right input').val();
     // il pulsante invia equivale a 13
         if(event.which == 13 ){
-            searchMovieSeriesTv();
+            searchMovieSeriesTv(ricercaUtente);
         };
     });
 
     // funzione per ricercare i film e serieTv
-    function searchMovieSeriesTv(ricercaUtente) {
-        // leggo il valore dell'input dela ricerca
-        var ricercaUtente = $('.header-right input').val();
+    function searchMovieSeriesTv(ricerca) {
         // se la ricerca non è vuota
-        if (ricercaUtente != 0) {
-            $('main p').text('Risultato ricerca: "' + ricercaUtente +'"')
-            $('.header-right input').val('');
-            $('.film-container').empty();
+        if (ricerca != 0) {
+            $('main p').text('Risultato ricerca: "' + ricerca +'"')
             // creo una variabile per ajax film
-            var ajaxMovie = $.ajax({
-                        // uso url della api di themoviedb
-                        'url': 'https://api.themoviedb.org/3/search/movie',
-                        // allego questi dati all'url
-                        'data': {
-                            // la mia chiave api
-                            'api_key': '8f20fb07349b7328a5884d56f17b612d',
-                            // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
-                            'query': ricercaUtente,
-                            // la lingua italiana
-                            'language': 'it-IT'
-                        },
-                        'method': 'GET',
-                        'success': function(data) {
-                            // richiamo la funzione delle informazione del film
-                            infoMovie(data.results);
-                        },
-                        // in caso di errore
-                        'error': function() {
-                            alert('Error')
-                        }
-                    });
+            $.ajax({
+                // uso url della api di themoviedb
+                'url': 'https://api.themoviedb.org/3/search/movie/',
+                // allego questi dati all'url
+                'data': {
+                    // la mia chiave api
+                    'api_key': '8f20fb07349b7328a5884d56f17b612d',
+                    // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
+                    'query': ricerca,
+                    // la lingua italiana
+                    'language': 'it-IT'
+                },
+                'method': 'GET',
+                'success': function(data) {
+                    // richiamo la funzione delle informazione del film
+                    infoMovie(data.results);
+                    $('.header-right input').val('');
+                },
+                // in caso di errore
+                'error': function() {
+                    alert('Error')
+                }
+            });
             // creo una variabile per ajax serietv
-            var ajaxSeriesTv = $.ajax({
-                        // uso url della api di themoviedb
-                        'url': 'https://api.themoviedb.org/3/search/tv',
-                        // allego questi dati all'url
-                        'data': {
-                            // la mia chiave api
-                            'api_key': '8f20fb07349b7328a5884d56f17b612d',
-                            // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
-                            'query': ricercaUtente,
-                            // la lingua italiana
-                            'language': 'it-IT'
-                        },
-                        'method': 'GET',
-                        'success': function(data) {
-                            // richiamo la funzione delle informazione della serie tv
-                            infoSeriesTv(data.results);
-                            console.log(data.results);
-                        },
-                        // in caso di errore
-                        'error': function() {
-                            alert('Error')
-                        }
-                    });
+            $.ajax({
+                // uso url della api di themoviedb
+                'url': 'https://api.themoviedb.org/3/search/tv/',
+                // allego questi dati all'url
+                'data': {
+                    // la mia chiave api
+                    'api_key': '8f20fb07349b7328a5884d56f17b612d',
+                    // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
+                    'query': ricerca,
+                    // la lingua italiana
+                    'language': 'it-IT'
+                },
+                'method': 'GET',
+                'success': function(data) {
+                    // richiamo la funzione delle informazione della serie tv
+                    var dataFilm = data.results;
+                    infoSeriesTv(dataFilm);
+                    $('.header-right input').val('');
+                },
+                // in caso di errore
+                'error': function() {
+                    alert('Error')
+                }
+            });
         // se la ricerca è vuota faccio comparire questo alert
         } else {
             $('main p').text('Inserisci un titolo di un film o serie tv.');
         }
     };
 
-    // funzione per ricercare le serieTv
-    // function searchSeriesTV(ricercaUtente) {
-    //     var ricercaUtente = $('.header-right input').val();
-    //     if (ricercaUtente != 0) {
-    //         $('.header-right input').val('');
-    //         $.ajax({
-    //             // uso url della api di themoviedb
-    //             'url': 'https://api.themoviedb.org/3/search/tv',
-    //             // allego questi dati all'url
-    //             'data': {
-    //                 // la mia chiave api
-    //                 'api_key': '8f20fb07349b7328a5884d56f17b612d',
-    //                 // il query, che sarebbe il titolo del film, in questo caso quelo che scrive l'utente
-    //                 'query': ricercaUtente,
-    //                 // la lingua italiana
-    //                 'language': 'it-IT'
-    //             },
-    //             'method': 'GET',
-    //             'success': function(data) {
-    //                 // richiamo la funzione delle informazione della serie tv
-    //                 infoSeriesTv(data.results);
-    //                 console.log(data.results);
-    //             },
-    //             // in caso di errore
-    //             'error': function() {
-    //                 alert('Error')
-    //             }
-    //         });
-    //     } else {
-    //         alert('Inserisci un titolo.')
-    //     }
-    // };
-
     // creo una funzione per estrarre le informazione dei film dall'api
     function infoMovie(movie) {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
+        var templateFunction = Handlebars.compile($('#template').html());
         for (var i = 0; i < movie.length; i++) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-            // if (movie[i].poster_path != null){
             var title = movie[i].title;
             var originalTitle = movie[i].original_title;
             var language = movie[i].original_language;
@@ -123,12 +92,12 @@ $(document).ready(function() {
             console.log(voto);
             var stellaPiena = '<i class="fas fa-star"></i>';
             var stellaVuota = '<i class="far fa-star"></i>';
-            var stellePiene = '';
+            var stelle = '';
                 for (var j = 0; j < 5; j++) {
                     if (j < voto) {
-                        stellePiene = stellePiene + stellaPiena;
+                        stelle = stelle + stellaPiena;
                     } else {
-                        stellePiene = stellePiene + stellaVuota;
+                        stelle = stelle + stellaVuota;
                     }
                 };
             var img = movie[i].poster_path;
@@ -138,7 +107,7 @@ $(document).ready(function() {
                 'title': title,
                 'original-title': originalTitle,
                 'language': language,
-                'vote': stellePiene,
+                'vote': stelle,
                 'poster': img,
                 'overview': description
             };
@@ -150,28 +119,30 @@ $(document).ready(function() {
     };
 
     // creo una funzione per estrarre le informazione delle serieTv dall'api
-    function infoSeriesTv(seriesTv) {
+    function infoSeriesTv(serieTv) {
+        var templateFunction = Handlebars.compile($('#template').html());
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
-        for (var i = 0; i < seriesTv.length; i++) {
+        for (var i = 0; i < serieTv.length; i++) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-            var title = seriesTv[i].name;
-            var originalTitle = seriesTv[i].original_name;
-            var language = seriesTv[i].original_language;
+            var title = serieTv[i].name;
+            var originalTitle = serieTv[i].original_name;
+            var language = serieTv[i].original_language;
             // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
-            var voto = Math.ceil((seriesTv[i].vote_average) / 2);
+            var voto = Math.ceil((serieTv[i].vote_average) / 2);
             console.log(voto);
             var stellaPiena = '<i class="fas fa-star"></i>';
             var stellaVuota = '<i class="far fa-star"></i>';
-            var stelle;
+            var stelle = '';
+            console.log(stelle);
             for (var j = 0; j < 5; j++) {
-                if (j > voto) {
+                if (j < voto) {
                     stelle = stelle + stellaPiena;
                 } else {
                     stelle = stelle + stellaVuota;
                 }
             };
-            var img = seriesTv[i].poster_path;
-            var description = seriesTv[i].overview;
+            var img = serieTv[i].poster_path;
+            var description = serieTv[i].overview;
             // info da sostituire nel mio handlebars template
             var info = {
                 'title': title,
