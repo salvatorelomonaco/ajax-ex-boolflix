16c8e890df0b059e9a43155c5e07bdffd744d987
salvatorelomonaco <lommetto95@gmail.com>
2019-12-17 14:18:11 +0100

    miglioramenti , mancano le bandierine e le stelline

--- main.js
+++ main.js
@@ -113,9 +113,7 @@ $(document).ready(function() {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
         for (var i = 0; i < movie.length; i++) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-            // if (movie[i].poster_path == null){
-            //     $('.film').hide();
-            // } else {
+            if (movie[i].poster_path != null){
                 var title = movie[i].title;
                 var originalTitle = movie[i].original_title;
                 var language = movie[i].original_language;
@@ -136,6 +134,10 @@ $(document).ready(function() {
                 var html = templateFunction(info);
                 //  le appendo al container
                 $('.film-container').append(html);
+
+            } else {
+                !$('.film-container').append(html);
+            }
         };
     };
 
@@ -143,27 +145,32 @@ $(document).ready(function() {
     function infoSeriesTv(seriesTv) {
         // uso un ciclo for visto che mi verrà restituita un array di oggetti
         for (var i = 0; i < seriesTv.length; i++) {
+            if (seriesTv[i].poster_path != null) {
             // creo le varie variabili per andare a prendere i dati di cui ho bisogno
-            var title = seriesTv[i].name;
-            var originalTitle = seriesTv[i].original_name;
-            var language = seriesTv[i].original_language;
-            // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
-            var vote = (Math.ceil(seriesTv[i].vote_average)) / 2;
-            var img = seriesTv[i].poster_path;
-            var description = seriesTv[i].overview;
-            // info da sostituire nel mio handlebars template
-            var info = {
-                'title': title,
-                'original-title': originalTitle,
-                'language': language,
-                'vote': vote,
-                'poster': img,
-                'overview': description
-            };
-            // creo una variabile che mi compili le info con una funzione
-            var html = templateFunction(info);
-            //  le appendo al container
-            $('.film-container').append(html);
+                var title = seriesTv[i].name;
+                var originalTitle = seriesTv[i].original_name;
+                var language = seriesTv[i].original_language;
+                // arrotondo per eccesso il numero della votazione e diviso per due per fare la votazione da 0 a 5
+                var vote = (Math.ceil(seriesTv[i].vote_average)) / 2;
+                var img = seriesTv[i].poster_path;
+                var description = seriesTv[i].overview;
+                // info da sostituire nel mio handlebars template
+                var info = {
+                    'title': title,
+                    'original-title': originalTitle,
+                    'language': language,
+                    'vote': vote,
+                    'poster': img,
+                    'overview': description
+                };
+                // creo una variabile che mi compili le info con una funzione
+                var html = templateFunction(info);
+                //  le appendo al container
+                $('.film-container').append(html);
+
+            } else {
+                !$('.film-container').append(html);
+            }
         };
-    }
+    };
 });
